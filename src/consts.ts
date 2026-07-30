07a6bff591f9c6c5608b323b488295d15e2231ab
Lecyn <sparkzure@163.com>
2026-07-30 20:05:56 +0800

    chore: 修改title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Lecyn's Blog";
+export const SITE_DESCRIPTION = "一个普通的学生，爱好自己感兴趣的世界！";
