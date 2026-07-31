b458c895ba49f98b042b73f163ab439777a5e340
Lecyn <sparkzure@163.com>
2026-07-31 17:37:47 +0800

    chore: 添加RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,15 +3,25 @@ import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection("blog");
+	const blogPosts = await getCollection("blog");
+	const shares = await getCollection("shares");
+
+	const allItems = [
+		...blogPosts.map((post) => ({
+			...post.data,
+			link: `/blog/${post.id}/`,
+		})),
+		...shares.map((share) => ({
+			...share.data,
+			link: `/share/${share.id}/`,
+		})),
+	].sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf());
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		customData: '<language>zh-CN</language>',
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+		items: allItems,
 	});
 }
