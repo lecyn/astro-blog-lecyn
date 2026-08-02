114a9f9d175cb5e60e97d1a37fb4bcabb440fa17
Lecyn <sparkzure@163.com>
2026-08-02 14:06:20 +0800

    feat: SEO优化

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Lecyn's Blog";
-export const SITE_DESCRIPTION = "一个普通的学生，爱好自己感兴趣的世界！";
+export const SITE_DESCRIPTION = "一个热爱代码的学生新手，记录编程学习、技术探索与个人成长。分享前端开发、博客搭建、开源项目等心得。";
+export const SITE_KEYWORDS = "编程,代码,前端,学生,新手,博客,技术,学习,个人成长";
+export const SITE_AUTHOR = "Lecyn";
 export const SITE_LOGO = "/font-image.png";
