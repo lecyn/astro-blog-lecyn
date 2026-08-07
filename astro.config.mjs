4e16a2b236eda5531def8bea1e9e84d4c039786d
Lecyn <sparkzure@163.com>
2026-08-07 19:17:53 +0800

    feat: 添加脚注样式和Markdown配置，优化文档展示

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import remarkGfm from "remark-gfm";
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -9,6 +10,9 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
 	site: "https://blog.lecyn.cn",
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkGfm],
+	},
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
